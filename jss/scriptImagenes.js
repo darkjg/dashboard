5b89a49e233e83fcf69cd718694b54a3b09f7ccf
Jorge <newdarkjg@gmail.com>
2023-12-18 19:07:52 +0100

    imagenees pages

--- jss/scriptImagenes.js
+++ jss/scriptImagenes.js
@@ -1,6 +1,6 @@
 
 let intervalImg = setInterval(CambiarImagen, 15000);
-let carpeta = "../img/ImagenesAleatorias";
+let carpeta = "./img/ImagenesAleatorias";
 let arrayImagenes=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
 function CambiarImagen(){
     let valorImagen= Math.floor(Math.random() * (arrayImagenes.length - 0) + 0);
